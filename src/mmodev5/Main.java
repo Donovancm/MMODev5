c9a614d37badd650b902837b23759e4421cb0ed4
Donovan Martodikromo <donovan.dcm@gmail.com>
2016-01-18 19:54:23 +0100

    Restart assignment1

---
+++ src/mmodev5/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mmodev5;
+
+import View.MainScreen;
+
+/**
+ *
+ * @author Donovan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainScreen mainscreen = new MainScreen();
+        mainscreen.setVisible(true);
+        mainscreen.setLocationRelativeTo(null);
+    }
+    
+}
